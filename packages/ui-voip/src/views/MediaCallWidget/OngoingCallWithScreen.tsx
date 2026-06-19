ab14e80808ca2e442ebe4b4886075340e07f9283
Guilherme Gazzo <5263975+ggazzo@users.noreply.github.com>
2026-06-19 11:02:08 -0300

    feat(sidebar): add SidebarRail collapsed icon navigation rail (#40558)

--- packages/ui-voip/src/views/MediaCallWidget/OngoingCallWithScreen.tsx
+++ packages/ui-voip/src/views/MediaCallWidget/OngoingCallWithScreen.tsx
@@ -1,6 +1,7 @@
 import { Box, ButtonGroup } from '@rocket.chat/fuselage';
 import { useTranslation } from 'react-i18next';
 
+import MediaCallDialpad from './MediaCallDialpad';
 import {
 	ToggleButton,
 	PeerInfo,
@@ -66,6 +67,8 @@ const OngoingCall = () => {
 			<WidgetContent>
 				<CardWidgetContainer>
 					<PeerInfo {...peerInfo} slots={remoteSlots} remoteMuted={remoteMuted} />
+
+					<MediaCallDialpad />
 
 					{remoteScreen?.active && (
 						<StreamCard autoHeight maxHeight={120} onClickOpenInRoom={onClickDirectMessage}>
